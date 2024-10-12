cccc3a4029399c9128f0df3e534a0ebbbea8812e
Andre Soares <andreiarley@gmail.com>
2024-10-12 08:50:07 -0300

    update instances

--- src/solutions/AlgoritmoGenetico/alg_gen.py
+++ src/solutions/AlgoritmoGenetico/alg_gen.py
@@ -1,8 +1,10 @@
-
+from src.instances.intance import Instance
 
 class TSP_AlgoritmoGenetico():
 
     def start(self):
+
+        distances = Instance().getAllDistances()
 
         print('\n----------------------- Resultados com Algoritmo Genético ----------------------')
         print('\n desenvolvendo \n ')
